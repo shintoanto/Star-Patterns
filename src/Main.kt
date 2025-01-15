d74233c0b4514a7c9a4822651b6bccbf21f68fc3
shinto pa <shintopa@outlook.com>
2025-01-15 17:26:09 +0530

    Aded message

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var pattern = Patterns();
+    val pattern = Patterns();
     pattern.rightHalfPyramid()
 
 }
@@ -9,11 +9,13 @@ fun main() {
 
 class Patterns{
     fun rightHalfPyramid(){
+        println("1 Right Half Pattern")
         for (i in 0..5){
             for (x in 0..i){
                 print("*")
             }
             println()
         }
+        println("Right Half Pattern END")
     }
 }
