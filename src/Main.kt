5eecfefcb488000ee6f5f1d26fbf56a4efa9eb6a
shinto pa <shintopa@outlook.com>
2025-01-15 17:27:37 +0530

    Aded message

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val pattern = Patterns();
+    val pattern = Patterns()
     pattern.rightHalfPyramid()
 
 }
